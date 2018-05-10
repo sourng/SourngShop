1cd5fd2b01c46ec04367fc791fe18f031cb0895b
SENG Sourng <sourng2009@gmail.com>
2018-05-10 14:37:10 +0700

    update

--- ci_ecom/application/views/front/home.php
+++ ci_ecom/application/views/front/home.php
@@ -64,16 +64,16 @@
                 <div class="col-md-12">
                     <div id="main-slider">
                         <div class="item">
-                            <img src="<?php echo base_url(); ?>uploads/product/img/main-slider1.jpg" alt="" class="img-responsive">
+                            <img src="<?php echo base_url(); ?>uploads/product/main-slider1.jpg" alt="" class="img-responsive">
                         </div>
                         <div class="item">
-                            <img class="img-responsive" src="<?php echo base_url(); ?>uploads/product/img/main-slider2.jpg" alt="">
+                            <img class="img-responsive" src="<?php echo base_url(); ?>uploads/product/main-slider2.jpg" alt="">
                         </div>
                         <div class="item">
-                            <img class="img-responsive" src="<?php echo base_url(); ?>uploads/product/img/main-slider3.jpg" alt="">
+                            <img class="img-responsive" src="<?php echo base_url(); ?>uploads/product/main-slider3.jpg" alt="">
                         </div>
                         <div class="item">
-                            <img class="img-responsive" src="<?php echo base_url(); ?>uploads/product/img/main-slider4.jpg" alt="">
+                            <img class="img-responsive" src="<?php echo base_url(); ?>uploads/product/main-slider4.jpg" alt="">
                         </div>
                     </div>
                     <!-- /#main-slider -->
